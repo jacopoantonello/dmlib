6d92cd8a5a839dae77312e5e61227e2b7ac67759
Jacopo Antonello <jacopo.antonello@dpag.ox.ac.uk>
2018-09-20 18:21:35 +0100

    Use absolute import

--- dmlib/__init__.py
+++ dmlib/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from . import version
+from dmlib import version
 
 __author__ = 'J Antonello, A. Barbotin'
 __copyright__ = 'Copyright 2018, J. Antonello, A. Barbotin'
