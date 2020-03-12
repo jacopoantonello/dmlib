e63333136c31ef0df84b8fcda52fb44d2ca4d014
Jacopo Antonello <jacopo@antonello.org>
2020-03-12 13:51:13 +0000

    Fix imread yet again

--- dmlib/test/__init__.py
+++ dmlib/test/__init__.py
@@ -3,14 +3,14 @@
 
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
+from imageio import imread
 from numpy.random import uniform
 from skimage.transform import resize
 
 
 def load_int3(shape):
-    img = plt.imread(os.path.join(
+    img = imread(os.path.join(
         os.path.dirname(__file__), 'int3.tif'))
     img = np.roll(img, int(np.round(uniform(-200, 200))), axis=0)
     img = np.roll(img, int(np.round(uniform(-200, 200))), axis=1)
