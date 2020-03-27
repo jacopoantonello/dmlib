9321763d5a575f29c91f78ba6099ccc9f13c459c
Jacopo Antonello <jacopo@antonello.org>
2020-03-27 16:32:52 +0000

    Remove hash from version numbering

--- setup.py
+++ setup.py
@@ -21,9 +21,7 @@ def update_version():
         toks = check_output(
             'git describe --tags --long --dirty', universal_newlines=True,
             shell=True).strip().split('-')
-        version = toks[0].strip('v') + '+' + toks[1] + '.' + toks[2]
-        if toks[-1] == 'dirty':
-            version += '.dirty'
+        version = toks[0].strip('v') + '.' + toks[1]
         last = check_output(
             'git log -n 1', universal_newlines=True, shell=True)
         date = re.search(
@@ -56,11 +54,13 @@ setup(
     version=lookup_version(),
     description='Python tools for deformable mirror calibration',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/jacopoantonello/dmlib',
     author='The DMLib Project Contributors',
     author_email='jacopo@antonello.org',
     license='GPLv3+',
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Physics',
         (
