cebcb4741fb5f92c931d4fa422dfda21eebc98ec
Jacopo Antonello <jacopo@antonello.org>
2020-03-27 16:17:26 +0000

    Fix package __version__ visibility

--- dmlib/__init__.py
+++ dmlib/__init__.py
@@ -11,6 +11,9 @@ __status__ = 'Prototype'
 __all__ = [
     'calibration', 'control', 'core', 'dmplot', 'gui', 'interf',
     'version', 'zpanel']
+__version__ = version.__version__
+__date__ = version.__date__
+__commit__ = version.__commit__
 __doc__ = """
 To be defined.
 
@@ -20,6 +23,6 @@ version: {}
 commit:  {}
 """.format(
     __author__,
-    version.__date__,
-    version.__version__,
-    version.__commit__)
+    __date__,
+    __version__,
+    __commit__)
